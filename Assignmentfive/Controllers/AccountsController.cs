ae6e80b426ca0c4c090f54ba031bc8c0a1da914b
Shylaja2021 <shylaja.r@jktech.com>
2022-04-18 19:52:12 +0530

    Add project files.

---
+++ Assignmentfive/Controllers/AccountsController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assignmentfive.Models;
+using System.Web.Security;
+namespace Assignmentfive.Controllers
+{
+    public class AccountsController : Controller
+    {
+        // GET: Accounts
+        private Assignment5Entities db = new Assignment5Entities();
+        public JsonResult IsUserNameAvailable(string username)
+        {
+
+            return Json(!db.Users.Any(u => u.UserName == username), JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(UserModel model)
+        {
+            using (Assignment5Entities entities = new Assignment5Entities())
+            {
+                bool isValidUser = entities.Users.Any(u => u.UserName.ToLower() == model.UserName.ToLower() && u.UserPassword == model.UserPassword);
+                if (isValidUser)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "Books");
+                }
+                ModelState.AddModelError("", "Invalid username or password");
+                return View();
+            }
+
+        }
+
+        public ActionResult SignUp()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(User model)
+        {
+            using (Assignment5Entities entities = new Assignment5Entities())
+            {
+                entities.Users.Add(model);
+                entities.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Assignmentfive/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Assignmentfive.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignmentfive.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        private Assignment5Entities db = new Assignment5Entities();
+        public JsonResult IsUserNameAvailable(string username)
+        {
+
+            return Json(!db.Users.Any(u => u.UserName == username), JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
